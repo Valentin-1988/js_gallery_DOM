d59d8594646beedf7f31caaf6127e4168a2a5b72
Valentin_Kuianov <kuyanov1988@mail.com>
2024-08-21 07:20:25 +0300

    make build

---
+++ index.7f28d056.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__list");e.addEventListener("click",function(t){t.preventDefault();var r=document.querySelector("#largeImg"),c=t.target.closest("a");c&&e.contains(c)&&(r.src=c.href)});
+//# sourceMappingURL=index.7f28d056.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,12 @@
+"use strict";
+const thumbCollection = document.querySelector(".gallery__list");
+thumbCollection.addEventListener("click", function(e) {
+    e.preventDefault();
+    const largeImg = document.querySelector("#largeImg");
+    const target = e.target.closest("a");
+    if (!target) return;
+    if (!thumbCollection.contains(target)) return;
+    largeImg.src = target.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
